3eb25697df8aa31a1223a8797c923d7652ce8519
sparrow_nethaji <nethajicharvitha12345@gmail.com>
2024-05-30 13:26:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//caculator program
+const display = document.getElementById("display");
+
+function appendTodisplay(input){
+        display.value += input;
+}
+
+function cleardisplay(){
+    display.value ="";
+
+}
+
+function calculate(){   
+    try{
+        display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="error";
+    }
+}
